12d8af5c853460acf3db88a39057785abe5833d5
crinis <postmaster@crinis.org>
2019-11-10 23:02:30 +0100

    Remove two unused files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Builds TYPO3 Docker images locally
-
-set -e
-
-docker build -t crinis/sourcebans:latest .
--- test.sh
+++
@@ -1,5 +0,0 @@
-for git_tag in $(git tag); do
-  git checkout tags/$git_tag
-  docker build -t $IMAGE_REPOSITORY:$git_tag .
-  docker push $IMAGE_REPOSITORY:$git_tag
-done
