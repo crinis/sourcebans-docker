55c7fa384f99bcf5e6c3338a3bd225bbaa085cdb
crinis <code@crinis.org>
2024-12-16 17:45:04 +0100

    Make local build tool configurable

--- build.sh
+++ build.sh
@@ -4,6 +4,18 @@
 
 set -e
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <docker|podman>"
+    exit 1
+fi
+
+TOOL=$1
+
+if [ "$TOOL" != "docker" ] && [ "$TOOL" != "podman" ]; then
+    echo "Invalid argument: $TOOL. Use 'docker' or 'podman'."
+    exit 1
+fi
+
 function get_latest_release_tag() {
     SB_TAG=$(curl https://api.github.com/repos/sbpp/sourcebans-pp/releases -s | jq -r ".[].tag_name" | grep '^1\.7\.[0-9]*$' -m1)
     echo $SB_TAG
@@ -11,5 +23,5 @@ function get_latest_release_tag() {
 
 SB_TAG=$(get_latest_release_tag)
 
-podman build -t crinis/sourcebans:sb-${SB_TAG} -t crinis/sourcebans:latest --build-arg CHECKOUT="${SB_TAG}" .
-podman build -t crinis/sourcebans:sb-dev --build-arg CHECKOUT="php81" .
+$TOOL build -t crinis/sourcebans:sb-${SB_TAG} -t crinis/sourcebans:latest --build-arg CHECKOUT="${SB_TAG}" .
+$TOOL build -t crinis/sourcebans:sb-dev --build-arg CHECKOUT="php81" .
